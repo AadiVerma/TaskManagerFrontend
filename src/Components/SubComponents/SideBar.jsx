b1939b7cd887508fc00fb2ad00b35a0d1e368c6b
Aditya Verma <122523624+AadiVerma@users.noreply.github.com>
2024-05-17 20:49:41 +0530

    Update SideBar.jsx

--- src/Components/SubComponents/SideBar.jsx
+++ src/Components/SubComponents/SideBar.jsx
@@ -12,10 +12,10 @@ export default function SideBar(){
           <img src={Image} alt="Image" />
           </div>
             <div className="mt-[30%]">
-                <h1 className="mb-4 text-lg font-newfont font-bold flex gap-4 hover:text-blue-600 cursor-pointer hover:bg-[#121212] p-4 rounded-lg"><LuLayoutDashboard className="text-xl mt-1"/> All Tasks</h1>
-                <h1 className="mb-4 text-lg font-newfont font-bold flex gap-4 hover:text-blue-600 cursor-pointer hover:bg-[#121212] p-4 rounded-lg"><IoGitNetwork className="text-xl mt-1"/> Important</h1>
-                <h1 className="mb-4 text-lg font-newfont font-bold flex gap-4 hover:text-blue-600 cursor-pointer hover:bg-[#121212] p-4 rounded-lg"><IoCloudDoneSharp className="text-xl mt-1"/> Completed</h1>
-                <h1 className="mb-4 text-lg font-newfont font-bold flex gap-4 hover:text-blue-600 cursor-pointer hover:bg-[#121212] p-4 rounded-lg"><GiProgression className="text-xl mt-1"/>Progress</h1>
+                <h1 className="mb-4 text-lg font-newfont font-bold flex gap-4 hover:text-blue-600 cursor-pointer hover:bg-[#121212] p-4 rounded-lg"><LuLayoutDashboard className="text-2xl mt-1"/> All Tasks</h1>
+                <h1 className="mb-4 text-lg font-newfont font-bold flex gap-4 hover:text-blue-600 cursor-pointer hover:bg-[#121212] p-4 rounded-lg"><IoGitNetwork className="text-2xl mt-1"/> Important</h1>
+                <h1 className="mb-4 text-lg font-newfont font-bold flex gap-4 hover:text-blue-600 cursor-pointer hover:bg-[#121212] p-4 rounded-lg"><IoCloudDoneSharp className="text-2xl mt-1"/> Completed</h1>
+                <h1 className="mb-4 text-lg font-newfont font-bold flex gap-4 hover:text-blue-600 cursor-pointer hover:bg-[#121212] p-4 rounded-lg"><GiProgression className="text-2xl mt-1"/>Progress</h1>
             </div>
             <div className="mt-[40%]">
            <h1 className="mb-4 text-xl font-newfont font-bold flex gap-4 hover:text-blue-600 cursor-pointer hover:bg-[#121212] p-4 rounded-lg"><LuLogOut className="text-3xl mt-1"/>Log Out</h1>
